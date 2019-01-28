d5ceb41518361e9e9d16ef28d9a44818955627c7
Nitesh kumar <30889903+smartnitesh@users.noreply.github.com>
2019-01-28 20:27:01 +0530

    Print Stars Using Loop

---
+++ Stars.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Stars
+{
+public static void main(String args[]){
+int r = 15; // for rows
+
+for(int i = 0;i<=r;i++)
+{
+    for(int st = 0; st<i; st++) //st rep.. stars
+    
+    {
+    System.out.print("*");
+    } 
+    System.out.println("");
+    
+} 
+    for(int m=1;m<=15;m++){
+     
+         for( int n=m;n<=15;n++){
+             
+             System.out.print("*");
+  }
+       System.out.println(" ");  
+     
+    }
+}   
+
+    }
