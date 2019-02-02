b3497b94153d3cb65d1992cf25fc17f8e2b08a99
Nitesh kumar <30889903+smartnitesh@users.noreply.github.com>
2019-02-02 20:50:45 +0530

    Even Odd

---
+++ Even.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.Scanner;
+class Even{
+    public static void main(String args[]){
+        int x;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter your nimber");
+        x= sc.nextInt();
+        if(x%2==0){
+            
+            System.out.print("even");
+        
+        }
+        else {
+            System.out.print("odd");
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+}
